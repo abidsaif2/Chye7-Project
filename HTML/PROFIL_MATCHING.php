bb6afed571379d4c5cd8a8a2f6cad157e35434cb
ayoub_jouini <ayoubjouini135@gmail.com>
2021-04-25 00:02:36 +0100

    matching final

--- HTML/PROFIL_MATCHING.php
+++ HTML/PROFIL_MATCHING.php
@@ -20,24 +20,16 @@
 
     </header>
     <div id="main">
-        <section1 class="section1">
-
-        </section1>
-
 
         <section2 class="section2">
 
             <div class="bg_all">
 
                 <div class="sec_place">
-                    <?php matching($_SESSION["email"]) ?>
                     <div class="rounded_img_place"></div>
                     <div class="ano_name"><?php echo $username; ?></div>
                     <div class="comp_pour email"><?php echo $useremail; ?></div>
                     <div class="birth_date"><?php echo $userdate; ?></div>
-                    <div class="btn_bg">
-                        <div class="btn_txt">Continue</div>
-                    </div>
                 </div>
 
 
@@ -48,12 +40,6 @@
         </section2>
 
     </div>
-
-    <section class="sec3">
-        <div class="btn_bg_2">
-            <div class="btn_txt_2">Next Person</div>
-        </div>
-    </section>
 
     <footer>
         <div class="bg"></div>
