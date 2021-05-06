6641c83b8cf05cd43fc9bb9a304bc4f0b6f7f39c
USERNAME <MY_NAME@example.com>
2021-05-06 07:27:30 +0100

    Survey and responsivness Finished , Nekes Checkbox input nbadalou css et background ta SignUp responsive

--- js/quiz.js
+++ js/quiz.js
@@ -1,88 +1,70 @@
-var main_Section = document.getElementById("section1");
-
-var bg1 = document.createElement("div");
-bg1.className="bg1";
-   main_Section.appendChild(bg1);
-
-  var bg2 = document.createElement("div");
-  bg2.className="bg2";
-    bg1.appendChild(bg2);
+var main_Section = document.querySelector(".back2");
 
 var form = document.createElement("form");
 form.setAttribute("id","form")
 
-bg2.appendChild(form)
-
-var title = document.createElement("p");
-title.className="title_2";
-  form.appendChild(title)
-
-var form_check = document.createElement("div");
-form_check.className="form-check";
-form.appendChild(form_check)
-
-var input_element= document.createElement("input");
-  input_element.className="form-check-input radio_btn_input";
-    input_element.setAttribute('type', 'radio');
-    input_element.setAttribute('name', 'flexRadioDefault');
-    input_element.setAttribute('id', 'flexRadioDefault2');
-
-    form_check.appendChild(input_element);
-
-    var label_element= document.createElement("label");
-      label_element.className="form-check-label radio_btn";
-       input_element.setAttribute('for', 'flexRadioDefault');
-
-      form_check.appendChild(label_element);
-
-    var btn = document.createElement("div");
-      btn.className="btn_bg";
-        form.appendChild(btn);
-
-        var btn_Texte = document.createElement("div");
-        btn_Texte.className="btn_txt";
-        btn_Texte.setAttribute('id','btn_txt')
-          btn.appendChild(btn_Texte);
+main_Section.appendChild(form)
 
 function form_template(numb,question,options){
 
             form.innerHTML = `
-              <form id="form" action="quizData.php" method="POST">
+            <form id="form" action="quizData.php" method="POST">
 
-              <p class="title_2">${question}</p>
+            <div class="back3">
+                <p class="text">
+                    ${question}
+                </p>
+            </div>
+                             
+            <label class="checkbox">
+                <input type="checkbox" value="a" id="quiz_input" class="checkbox" name="quiz_input">
+                <span class="rounded">
+                    <span class="txtt">
+                          ${options[0]}
+                     </span>
+                </span>
+            </label>
 
-             <div class="form-check">
-                 <input class="form-check-input radio_btn_input" type="radio" name="q" id="flexRadioDefault1" value="a">
-                 <label class="form-check-label radio_btn" for="flexRadioDefault1">
-                    ${options[0]}
-                 </label>
-               </div>
-               <div class="form-check">
-                 <input class="form-check-input radio_btn_input" type="radio" name="q" id="flexRadioDefault2" value="b" checked>
-                 <label class="form-check-label radio_btn" for="flexRadioDefault2">
-                    ${options[1]}
-                 </label>
-               </div> 
+            <label class="checkbox">
+                <input type="checkbox" value="b" id="quiz_input" class="checkbox" name="quiz_input">
+                <span class="rounded">
+                    <span class="txtt">
+                          ${options[1]}
+                     </span>
+                </span>
+            </label>
 
-               <div class="form-check">
-                 <input class="form-check-input radio_btn_input" type="radio" name="q" id="flexRadioDefault2" value="c">
-                 <label class="form-check-label radio_btn" for="flexRadioDefault2">
-                    ${options[2]}
-                 </label>
-               </div> 
-             
-               <div class="form-check">
-                 <input class="form-check-input radio_btn_input" type="radio" name="q" id="flexRadioDefault2" value="d">
-                 <label class="form-check-label radio_btn" for="flexRadioDefault2">
-                   ${options[3]}
-                 </label>
-               </div> 
-             
-               <div class="btn_bg">
-                   <input class="btn_txt" id="btn_txt" type="submit" placeholder="Continue"onclick="next(event)"></input>
-               </div>
+            <label class="checkbox">
+                <input type="checkbox" value="c" id="quiz_input" class="checkbox" name="quiz_input">
+                <span class="rounded">
+                    <span class="txtt">
+                          ${options[2]}
+                     </span>
+                </span>
+            </label>
 
-             </form>`
+            <label class="checkbox">
+                <input type="checkbox" value="d" id="quiz_input" class="checkbox" name="quiz_input">
+                <span class="rounded">
+                    <span class="txtt">
+                          ${options[3]}
+                     </span>
+                </span>
+            </label>
+
+            <div class="btn_sub">
+                <button class="btn_txt" id="btn_txt" type="submit" onclick="next(event)">NEXT QUESTION</button>
+            </div>
+
+        </form>
+        
+        <script type="text/javascript">
+            $('.txtt').click(function() {
+                $(this).siblings('input:checkbox').prop('checked' , false);
+            });
+        </script>
+
+        `
           
    }
 
